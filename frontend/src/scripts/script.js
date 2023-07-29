8a942f74ad557a18b30a7788b0f655e75e8c8bb8
unknown <youssefhoussein701@gmail.com>
2023-07-29 15:37:55 +0300

    <frontend> add the js function to handle the input of the signup

--- frontend/src/scripts/script.js
+++ frontend/src/scripts/script.js
@@ -25,6 +25,51 @@ pages.page_index = () => {
    
 }
 
+pages.page_register = () => {
+    const first_name =pages.getElement("first-name-signup").value
+    const last_name = pages.getElement("last-name-signup").value
+    const email = pages.getElement("email-signup").value
+    const password = pages.getElement("password-signup").value
+    const role = pages.getElement("role")
+    const button_login = pages.getElement("button-register")
+    const main_container = pages.getElement("signup-container")
+    const model_h1 = pages.getElement("model-h1-signup")
+    const model_h4 = pages.getElement("model-h4-signup")
+    const back = pages.getElement("signup-back")
+    const model = pages.getElement("model-signup")
+    const choose = pages.getElement("role-choose")
+    const admin  =pages.getElement("admin")
+    const user = pages.getElement("user")
+    const role_choosed = false
+    button_login.addEventListener('click', function(){
+        if(!first_name || !last_name || !email || !password || !role_choosed){
+            main_container.style.display = "none"
+            model.style.display = "flex"
+            model_h1.innerHTML = "Warning !!"
+            model_h4.innerHTML = "You should fill all the fields"
+        }
+    })
+    back.addEventListener('click',function () {
+        window.location.href = "register.html"
+    })
+    role.addEventListener('click', function(){
+        choose.style.display = "block"
+
+        admin.addEventListener('click', function(event){
+            event.stopImmediatePropagation();
+            choose.style.display = "none"
+            role_choosed = true
+        })
+        user.addEventListener('click', function(event){
+            event.stopImmediatePropagation();
+            choose.style.display = "none"
+            role_choosed = true
+        })
+    })
+}
+pages.warningEmpty = () => {
+
+}
 pages.loadPage = (page) => {
     eval("pages.page_" + page + "();")
 }
